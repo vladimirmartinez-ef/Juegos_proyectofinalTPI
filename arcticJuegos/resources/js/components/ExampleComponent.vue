219c0121f5f22a7b7591e119741d6e2a93702ab6
VM18042 <vm18042@ues.edu.sv>
2021-05-03 19:42:00 -0600

    Agregados componet Principal para visualizar

--- arcticJuegos/resources/js/components/ExampleComponent.vue
+++ arcticJuegos/resources/js/components/ExampleComponent.vue
@@ -1,23 +1,39 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <v-app>
+        <v-navigation-drawer app clipped>
+            <v-list dense>
+                <v-list-item link to="/">
+                    <v-list-item-action>
+                        <v-icon>mdi-view-dashboard</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>TODO</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar app clipped-left>
+            <v-toolbar-title>
+                <v-icon color="blue" size="100px">mdi-gamepad-variant</v-icon
+                >Arctic Game Universe</v-toolbar-title
+            >
+        </v-app-bar>
+        <v-main>
+            <v-container fluid>
+                <router-view></router-view>
+            </v-container>
+        </v-main>
+        <v-footer app> </v-footer>
+    </v-app>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    },
+    created() {
+        this.$vuetify.theme.dark = true;
     }
+};
 </script>
